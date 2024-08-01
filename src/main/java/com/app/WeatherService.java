91b97c805637c62a9882789dccc5a066dd433457
Adam Niewiadomski <121-adam.niewiadomski@users.noreply.gitlab.antologic.info>
2024-08-01 10:05:01 +0200

    main: adding data streaming

--- src/main/java/com/app/WeatherService.java
+++ src/main/java/com/app/WeatherService.java
@@ -3,13 +3,19 @@ package com.app;
 import com.app.model.AvgTempPerYear;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+import org.apache.spark.sql.streaming.OutputMode;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.to_date;
@@ -20,49 +26,78 @@ import static org.apache.spark.sql.functions.year;
 public class WeatherService {
 
     // just to simplify we hardcode the file directory
-    private final String FILE_DIR = "src/main/resources/example_file.csv";
+    private final String FILE_DIR = "C:\\Users\\Adam\\Desktop\\big-file-reading\\src\\main\\resources";
     private final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
     private final String DATAFRAME_VIEW_NAME = "temperature_data";
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final SparkSession spark;
+    private final StructType sparkSchema = new StructType()
+            .add("city", DataTypes.StringType, false)
+            .add("date", DataTypes.StringType, false)
+            .add("temperature", DataTypes.DoubleType, false);
 
     @Autowired
-    public WeatherService(final SparkSession sparkSession) {
+    public WeatherService(final SparkSession sparkSession) throws TimeoutException, StreamingQueryException {
         this.spark = sparkSession;
+        startProcessingTemperatureData();
     }
 
     public String calculateAvgTemperaturesForCity(final String city) {
         try {
-            final List<AvgTempPerYear> yearlyTemperatures = createDataframeWithYearlyTemperatures(city);
+            final List<AvgTempPerYear> yearlyTemperatures = getAverageTemperature(city);
             return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(yearlyTemperatures);
         } catch (final Exception e) {
             return "An error occurred while calculating average temperatures. Error message: " + e.getMessage();
         }
     }
 
-    private List<AvgTempPerYear> createDataframeWithYearlyTemperatures(final String city) {
-        createDataframeWithRawData();
-        final Dataset<Row> averageTemperatures = spark.sql("SELECT year, round(avg(temperature), 1) as averageTemperature " +
-                "FROM " + DATAFRAME_VIEW_NAME + " " +
-                "WHERE upper(city) = '" + city.toUpperCase() + "'" +
-                "GROUP BY year " +
-                "ORDER BY year");
-
-        return averageTemperatures.as(Encoders.bean(AvgTempPerYear.class)).collectAsList();
+    public void startProcessingTemperatureData() throws TimeoutException, StreamingQueryException {
+        final Dataset<Row> dataStream = readDataStream();
+        final Dataset<Row> averageTemperatures = executeDataQuery(dataStream);
+        averageTemperatures.createOrReplaceTempView(DATAFRAME_VIEW_NAME);
+        writeDataStream(averageTemperatures);
     }
 
-    private void createDataframeWithRawData() {
-        Dataset<Row> df = spark.read()
+    private List<AvgTempPerYear> getAverageTemperature(final String city) {
+        final List<AvgTempPerYear> result = new ArrayList<>();
+        final Dataset<Row> averageTemperature = spark.sql("SELECT year, averageTemperature " +
+                "FROM " + DATAFRAME_VIEW_NAME + " " +
+                "WHERE upper(city) = '" + city.toUpperCase() + "' " +
+                "ORDER BY year");
+
+        for (final Row row : averageTemperature.collectAsList()) {
+            result.add(new AvgTempPerYear(row.getString(0), row.getDouble(1)));
+        }
+
+        return result;
+    }
+
+    private Dataset<Row> readDataStream() {
+        return spark.readStream()
                 .option("header", false)
                 .option("delimiter", ";")
+                .format("csv")
+                .schema(sparkSchema)
                 .csv(FILE_DIR)
                 .toDF("city", "date", "temperature")
                 .withColumn("date", to_date(col("date"), DATE_FORMAT))
                 .withColumn("year", year(col("date")))
                 .drop(col("date"))
                 .repartition(col("year"));
+    }
 
-        df.createOrReplaceTempView(DATAFRAME_VIEW_NAME);
+    private Dataset<Row> executeDataQuery(final Dataset<Row> dataStream) {
+        return dataStream.groupBy("year", "city")
+                .agg(functions.avg("temperature").alias("averageTemperature"));
+    }
+
+    private void writeDataStream(final Dataset<Row> averageTemperatures) throws TimeoutException, StreamingQueryException {
+        averageTemperatures.writeStream()
+                .format("memory")
+                .outputMode(OutputMode.Update())
+                .queryName(DATAFRAME_VIEW_NAME)
+                .start()
+                .awaitTermination();
     }
 }
